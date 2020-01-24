225449696be5c652132cc1720cd78361e5059f20
gAndrzejowski <g.andrzejowski@gmail.com>
2020-01-25 00:46:33 +0100

    TDL-7 - Edytuj istniejące todo (#5)

--- TodoListClasses/Lista.cs
+++ TodoListClasses/Lista.cs
@@ -11,8 +11,6 @@ namespace TodoListClasses
             todos = new List<Todo>();
             todos.Add(new Todo("Zadanie pierwsze"));
             todos.Add(new Todo("Zadanie drugie"));
-            todos.Add(new Todo("Zadanie które pominęło trzecie"));
-            todos.Add(new Todo("Zadanie piąte"));
             todos.Add(new Todo("Zadanie dodatkowe", DateTime.Today.AddDays(140), 1, "Bardzo ważne zadanie\nAż do przesady"));
         }
         public List<Todo> Todos
--- TodoListTests/ListaTest.cs
+++ TodoListTests/ListaTest.cs
@@ -14,5 +14,41 @@ namespace TodoListTests
             Lista listaTestowa = new Lista();
             Assert.AreNotEqual(listaTestowa.Todos.Count, 0);
         }
+
+        [TestMethod]
+        public void ZmodyfikujLubDodajTodoDodajeTodoDoListyDlaRoznychId()
+        {
+            Todo pierwsze = new Todo("Testowe1", DateTime.Now, 2, "", "To todo jest inne niż drugie");
+            Todo drugie = new Todo("Testowe1", DateTime.Now, 2, "", "Bo są takie same ale mają inne Id");
+
+            Lista testowa = new Lista();
+            testowa.Todos.Add(pierwsze);
+            testowa.ZmodyfikujLubDodajTodo(drugie);
+            
+            Assert.IsTrue(testowa.Todos.Contains(pierwsze));
+            Assert.IsTrue(testowa.Todos.Contains(drugie));
+
+        }
+
+        [TestMethod]
+        public void ZmodyfikujLubDodajTodoPodmieniaTodoOTymSamymIdWLiscie()
+        {
+            Todo pierwsze = new Todo("Testowe1", DateTime.Now, 2, "To jest to samo todo co drugie", "foo");
+            Todo drugie = new Todo("Testowe2", DateTime.Now, 2, "Bo są różne, ale Id mają to samo", "foo");
+            
+            Lista testowa = new Lista();
+            testowa.Todos.Add(pierwsze);
+            testowa.Todos.Add(new Todo());
+            testowa.Todos.Add(new Todo());
+            
+            int pozycjaPierwszego = testowa.Todos.FindIndex(todo => todo == pierwsze);
+            testowa.ZmodyfikujLubDodajTodo(drugie);
+            
+            Assert.IsFalse(testowa.Todos.Contains(pierwsze));
+            Assert.IsTrue(testowa.Todos.Contains(drugie));
+
+            int pozycjaDrugiego = testowa.Todos.FindIndex(todo => todo == drugie);
+            Assert.AreEqual(pozycjaDrugiego, pozycjaPierwszego);
+        }
     }
 }
